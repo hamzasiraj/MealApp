d1a6c578bbb4a1f11f70ca78d7ea02150b57f806
hamzasiraj <36099837+hamzasiraj@users.noreply.github.com>
2021-01-19 12:47:31 +0500

    Update categories_screen.dart

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,7 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: Text('Meals Categories'),
       ),
       body: GridView(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(30),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
